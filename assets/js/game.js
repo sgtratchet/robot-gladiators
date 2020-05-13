782fbc7a88273b42648bafecf3545866db4e9cdd
Devin ellsworth <ellsworths@Sabrinas-Mac-mini.local>
2020-05-12 19:51:30 -0600

    added robot name line

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,9 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("what is your robot's name?")
+function fight(){
+    window.alert('The fight has begun:');
+}
+//fight();
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+console.log(10+10);
+console.log("Our robot's name is " + playerName);
