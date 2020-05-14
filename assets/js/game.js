c67d981fd783dc70720f5e518325b561f86f6446
Devin Ellsworth <devin.ellsworth@power-innovations.com>
2020-05-14 12:49:37 -0600

    added debugger for now

--- assets/js/game.js
+++ assets/js/game.js
@@ -22,55 +22,60 @@ var playerMoney = 10;
 //   console.log(i);
 //   console.log(enemyNames[i] + " is at " + i + " index");
 // }
+window.alert("Welcome to Robot Gladiators!")
 
 var fight = function(enemyName){
-    window.alert("Welcome to Robot Gladiators!")
+  while(enemyHealth>0){
+    
 
-var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
-//Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
-if (promptFight === "fight" || promptFight ==="FIGHT"){
-  enemyHealth = enemyHealth - playerAttack;
-  // Log a resulting message to the console so we know that it worked.
-  console.log(
-    playerName + " attacked " + enemyName + ". " + enemyName +" now has " +enemyHealth + " health remaining.");
+  var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+  //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+  if (promptFight === "fight" || promptFight ==="FIGHT"){
+    enemyHealth = enemyHealth - playerAttack;
+    // Log a resulting message to the console so we know that it worked.
+    console.log(playerName + " attacked " + enemyName + ". " + enemyName +" now has " +enemyHealth + " health remaining.");
 
-  // check enemy's health
-  if (enemyHealth <= 0) {
-    window.alert(enemyName + " has died!");
-  } 
-  else {
-    window.alert(enemyName + " still has " + enemyHealth + " health left.");
-  }
-  // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
-  playerHealth = playerHealth - enemyAttack;
-  // Log a resulting message to the console so we know that it worked.
-  console.log(
-    enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
-  );
-  //check players health
-  if (playerHealth <= 0) {
-    window.alert(playerName + " has died!");
-  } 
-  else {
-    window.alert(playerName + " still has " + playerHealth + " health left.");
-  }
-} else if (promptFight ==="skip"||promptFight==="SKIP"){
-  var confirmSkip=window.confirm("are you sure you'd like to quit?");
+    // check enemy's health
+    if (enemyHealth <= 0) {
+      window.alert(enemyName + " has died!");
+    } 
+    else {
+      window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
+    // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+    playerHealth = playerHealth - enemyAttack;
+    // Log a resulting message to the console so we know that it worked.
+    console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+    );
+    //check players health
+    if (playerHealth <= 0) {
+      window.alert(playerName + " has died!");
+    } 
+    else {
+      window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
+    }
 
-//if yes, leave fight
-  if(confirmSkip){
-    window.alert(playerName+" has chosen to skip the fight, Goodbye!");
-    playerMoney=playerMoney - 2;
-  }
-  else{
-    fight();
-  }
+    else if (promptFight ==="skip"||promptFight==="SKIP"){
+        var confirmSkip=window.confirm("are you sure you'd like to quit?");
+
+    //if yes, leave fight
+    if(confirmSkip){
+      window.alert(playerName+" has chosen to skip the fight, Goodbye!");
+      playerMoney=playerMoney - 2;
+    }
+
+    else{
+      fight();
+    }
 } 
 else {
   window.alert("You need to pick a valid option. Try again!");
 }
-};
+}
+}
 
 for(var i=0; i < enemyNames.length; i++) {
+  debugger;
   fight(enemyNames[i]);
 }
