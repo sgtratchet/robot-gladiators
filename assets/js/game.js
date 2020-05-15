5963a88de1dc113a4864d43a67430a7f7cd6e7ce
Devin Ellsworth <devin.ellsworth@power-innovations.com>
2020-05-15 08:24:24 -0600

    finished MVP

--- assets/js/game.js
+++ assets/js/game.js
@@ -77,8 +77,15 @@ var fight = function(enemyName){
     }
   };
 for(var i=0; i < enemyNames.length; i++) {
+  if(playerHealth >0){
+    window.alert('Welcome to Robot Gladiators! Round '+(i+1));
   var pickedEnemyName = enemyNames[i];
   enemyHealth = 50;
   // debugger;
   fight(pickedEnemyName);
+  }
+  else{
+    window.alert("you have lost your robot in Combat! Game Over!");
+    break;
+  }
 }
