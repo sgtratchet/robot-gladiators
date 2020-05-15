79d525296cfd2098f008e3c26871c6d746e5f7b4
Devin Ellsworth <devin.ellsworth@power-innovations.com>
2020-05-15 13:54:31 -0600

    added variable damages

--- assets/js/game.js
+++ assets/js/game.js
@@ -34,14 +34,15 @@ var fight = function(enemyName){
       //if yes, leave fight
       if(confirmSkip){
        window.alert(playerName+" has chosen to skip the fight. Goodbye!");
-        playerMoney=playerMoney - 10;
+        playerMoney=Math.max(0,playerMoney - 10);
         console.log("playerMoney", playerMoney);
         break;
       }
       }
         if (promptFight === "fight" || promptFight ==="FIGHT"){
         //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
-        enemyHealth = enemyHealth - playerAttack;
+        var damage = randomNumber(playerAttack - 3, playerAttack);
+            enemyHealth = Math.max(0, enemyHealth - damage);
         // Log a resulting message to the console so we know that it worked.
         console.log(playerName + " attacked " + enemyName + ". " + enemyName +" now has " +enemyHealth + " health remaining.");
 
@@ -55,7 +56,8 @@ var fight = function(enemyName){
             window.alert(enemyName + " still has " + enemyHealth + " health left.");
           }
         // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
-        playerHealth = playerHealth - enemyAttack;
+        var damage = randomNumber(enemyAttack - 3, enemyAttack);
+            playerHealth = Math.max(0, playerHealth - damage);
         // Log a resulting message to the console so we know that it worked.
         console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
       );
@@ -78,6 +80,7 @@ var startGame = function(){
   playerHealth=100;
   playerAttack=10;
   playerMoney=10;
+  enemyHealth=randomNumber(40,60);
 
 var shop = function(){
   shopOptionPrompt = window.prompt("Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice.");
@@ -152,3 +155,9 @@ if(playAgainConfirm){
 else{
   window.alert("Thank you for playing Robot Gladiators! Come back soon!");
 }
+// function to generate a random numeric value
+var randomNumber = function(min,max) {
+  var value = Math.floor(Math.random() * (max-min+1)+min);
+
+  return value;
+};
